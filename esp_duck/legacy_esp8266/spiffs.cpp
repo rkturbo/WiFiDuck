1abdf779439eb6cff917313a2b45b619b5240db0
rkturbo <58132647+rkturbo@users.noreply.github.com>
2026-01-05 00:32:48 -0500

    Delete esp_duck/legacy_esp8266 directory

--- esp_duck/legacy_esp8266/spiffs.cpp
+++
@@ -1,7 +0,0 @@
-/*
-  Legacy ESP8266 spiffs.cpp moved from esp_duck/spiffs.cpp
-  Original source: https://github.com/rkturbo/WiFiDuck/blob/master/esp_duck/spiffs.cpp
-  This branch removes ESP8266-specific implementation; keep reference here for history.
-*/
-
-// Original content archived in repository history. See link above.
--- esp_duck/legacy_esp8266/webserver.cpp
+++
@@ -1,7 +0,0 @@
-/*
-  Legacy ESP8266 webserver.cpp moved from esp_duck/webserver.cpp
-  Original source: https://github.com/rkturbo/WiFiDuck/blob/master/esp_duck/webserver.cpp
-  This branch removes ESP8266-specific implementation; keep reference here for history.
-*/
-
-// Original content archived in repository history. See link above.
